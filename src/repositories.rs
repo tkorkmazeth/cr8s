1943bc2d365152fa077b9c3a2ce3df4c11e2d379
Muhammed Talha Korkmaz <kriptovekediler@gmail.com>
2024-09-09 22:00:29 +0300

    cruds

--- src/repositories.rs
+++ src/repositories.rs
@@ -1,12 +1,82 @@
-use diesel::{QueryDsl, QueryResult};
+use diesel::{ExpressionMethods, QueryDsl, QueryResult};
 use diesel_async::{AsyncPgConnection, RunQueryDsl};
 
-use crate::{models::Rustacean, schema::rustaceans};
+use crate::{
+    models::{Crate, NewCrate, NewRustacean, Rustacean},
+    schema::{crates, rustaceans},
+};
 
-pub struct RustaceansRepository;
+pub struct RustaceanRepository;
 
-impl RustaceansRepository {
+impl RustaceanRepository {
     pub async fn find(c: &mut AsyncPgConnection, id: i32) -> QueryResult<Rustacean> {
         rustaceans::table.find(id).get_result(c).await
+    }
+
+    pub async fn find_multiple(
+        c: &mut AsyncPgConnection,
+        limit: i64,
+    ) -> QueryResult<Vec<Rustacean>> {
+        rustaceans::table.limit(limit).get_results(c).await
+    }
+
+    pub async fn create(
+        c: &mut AsyncPgConnection,
+        new_rustacean: NewRustacean,
+    ) -> QueryResult<Rustacean> {
+        diesel::insert_into(rustaceans::table)
+            .values(new_rustacean)
+            .get_result(c)
+            .await
+    }
+
+    pub async fn update(c: &mut AsyncPgConnection, rustacean: Rustacean) -> QueryResult<Rustacean> {
+        diesel::update(rustaceans::table.find(rustacean.id))
+            .set((
+                rustaceans::name.eq(rustacean.name),
+                rustaceans::email.eq(rustacean.email),
+            ))
+            .get_result(c)
+            .await
+    }
+
+    pub async fn delete(c: &mut AsyncPgConnection, id: i32) -> QueryResult<usize> {
+        diesel::delete(rustaceans::table.find(id)).execute(c).await
+    }
+}
+
+pub struct CrateRepository;
+
+impl CrateRepository {
+    pub async fn find(c: &mut AsyncPgConnection, id: i32) -> QueryResult<Crate> {
+        crates::table.find(id).get_result(c).await
+    }
+
+    pub async fn find_multiple(c: &mut AsyncPgConnection, limit: i64) -> QueryResult<Vec<Crate>> {
+        crates::table.limit(limit).get_results(c).await
+    }
+
+    pub async fn create(c: &mut AsyncPgConnection, new_crate: NewCrate) -> QueryResult<Crate> {
+        diesel::insert_into(crates::table)
+            .values(new_crate)
+            .get_result(c)
+            .await
+    }
+
+    pub async fn update(c: &mut AsyncPgConnection, a_crate: Crate) -> QueryResult<Crate> {
+        diesel::update(crates::table.find(a_crate.id))
+            .set((
+                crates::name.eq(a_crate.name),
+                crates::rustacean_id.eq(a_crate.rustacean_id),
+                crates::code.eq(a_crate.code),
+                crates::description.eq(a_crate.description),
+                crates::version.eq(a_crate.version),
+            ))
+            .get_result(c)
+            .await
+    }
+
+    pub async fn delete(c: &mut AsyncPgConnection, id: i32) -> QueryResult<usize> {
+        diesel::delete(crates::table.find(id)).execute(c).await
     }
 }
